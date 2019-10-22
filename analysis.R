90da048cc4f44af5fa02e882bfb5a5c58d29a3e0
vgXhc <kliems@gmail.com>
2019-10-22 08:58:42 -0500

    wrote new function to sequentially pull different weather observations to deal with limit of 1000 data points per API call

--- analysis.R
+++ analysis.R
@@ -1,14 +1,42 @@
 library(tidyverse)
+library(purrr)
 library(lubridate)
 library(rnoaa)
 
 #weather station ID for CHARMANY FARM, WI station on Mineral Pt Rd
 id <- "GHCND:USC00471416"
 
-#get daily observation weather data; max date range is 1 year
-weather <- ncdc(datasetid='GHCND', stationid='GHCND:USC00471416',startdate ='2014-10-09', enddate ='2019-08-31', add_units = TRUE)
+#get daily observation weather data
+# max date range is 1 year, but there's also a 1000 records limit
+# with 6 values/day, you can only get about 160 days per call
+# solution: get each datatype and year separately
 
-##get data
+#define function to pull one year's worth of weather data  
+get_weather <- function(year, data_type) {
+  df <- ncdc(datasetid='GHCND', 
+             stationid='GHCND:USC00471416',
+             startdate = paste0(year, "-01-01"), 
+             enddate = paste0(year, "-12-31"), 
+             add_units = TRUE, 
+             limit = 1000,
+             datatypeid = data_type)
+  df$data
+}
+
+# list of years and data types
+combo <- list(year = c(2014:2019), id = c("PRCP", "SNOW", "SNWD", "TMAX", "TMIN", "TOBS"))
+#create list of all possible combinations; transpose so that map2 can digest it
+args <- combo %>%
+  cross() %>%
+  transpose()
+
+# map over args to get weather data
+weather <- map2_df(args[[1]], args[[2]], get_weather)  
+
+# some cleaning is necessary
+
+
+##get bike counter data
 cc_counts <- read_csv("https://opendata.arcgis.com/datasets/367cb53685c74628b4975d8f65d20748_0.csv", col_types = "ci-") %>% mutate(location = "Cap City at North Shore")
 sw_counts <- read_csv("https://opendata.arcgis.com/datasets/8860784eb30e4a45a6f853b5f81949f2_0.csv", col_types = "ci-") %>% mutate(location = "SW Path at Randall")
 #combine two counter locations
